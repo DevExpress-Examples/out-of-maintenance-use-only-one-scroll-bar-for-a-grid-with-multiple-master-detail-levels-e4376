065d94be06473af2de266fc53a5558d5bf3eaf39
Alexey Zelenov (DevExpress) <alexey.zelenov@devexpress.com>
2020-12-17 18:19:36 +0300

    update v16.2.2

--- CS/Form1.Designer.cs
+++ CS/Form1.Designer.cs
@@ -1,31 +1,5 @@
-﻿// Developer Express Code Central Example:
-// Use only one scroll bar for a grid with multiple master/detail levels.
-// 
-// A grid containing a number of groups (master), each with a number of lines
-// (detail) is a representation of an order which is grouped for readability. When
-// a group has a greater number of lines then fits on the screen, a scrollbar is
-// added to the detail-view and when there are multiple groups the master-view also
-// gets a scrollbar. When the user scrolls through the grid, only the master-view
-// is scrolled (unless a row is selected, then only the detail-view is scrolled)
-// and it feels like the program is behaving oddly, as half the rows are
-// skipped.
-// To achieve this goal, we need the GridControl to have a height equal
-// to the height of all content placed in a master-view. So, the scrollbar does not
-// appear in either the master-view or in the detail-view. To allow scrolling, we
-// need to put our GridControl into a XtraScrollableControl and set the
-// GridControl.Dock property to DockStyle.Top.
-// We created the MyGridControl class
-// - a descendant of the GridControl. MyGridControl works with MyGridView views and
-// includes the CalcGridHeight() method, which is called when the appearance of the
-// Master-View is changed.
-// To put MyGridControl in the XtraScrollableContainer,
-// use the MyGridControl.InitScrolling() method at runtime.
-// Please note that if
-// you want to change Bounds or Dock properties of MyGridControl at runtime, change
-// similar properties of the MyGridControl.ScrollableContainer control.
-// 
-// You can find sample updates and versions for different programming languages here:
-// http://www.devexpress.com/example=E4376
+﻿using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Views.Grid;
 
 namespace SingleScrollingGridControl
 {
@@ -57,40 +31,19 @@ namespace SingleScrollingGridControl
         /// </summary>
         private void InitializeComponent()
         {
-            this.myGridControl1 = new CustomGrid.MyGridControl();
-            this.myGridView1 = new CustomGrid.MyGridView();
-            this.myGridControl2 = new CustomGrid.MyGridControl();
-            this.myGridView6 = new CustomGrid.MyGridView();
-            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
-            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
-            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
-            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
-            this.layoutControlGroup2 = new DevExpress.XtraLayout.LayoutControlGroup();
-            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
-            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
-            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.myGridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.myGridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             ((System.ComponentModel.ISupportInitialize)(this.myGridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.myGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.myGridControl2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.myGridView6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
-            this.layoutControl1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
             this.SuspendLayout();
             // 
             // myGridControl1
             // 
             this.myGridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.myGridControl1.Location = new System.Drawing.Point(796, 0);
+            this.myGridControl1.Location = new System.Drawing.Point(0, 0);
             this.myGridControl1.MainView = this.myGridView1;
             this.myGridControl1.Name = "myGridControl1";
-            this.myGridControl1.Size = new System.Drawing.Size(534, 474);
+            this.myGridControl1.Size = new System.Drawing.Size(504, 407);
             this.myGridControl1.TabIndex = 0;
             this.myGridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.myGridView1});
@@ -99,154 +52,27 @@ namespace SingleScrollingGridControl
             // 
             this.myGridView1.GridControl = this.myGridControl1;
             this.myGridView1.Name = "myGridView1";
-            this.myGridView1.OptionsView.ShowHorizontalLines = DevExpress.Utils.DefaultBoolean.True;
-            this.myGridView1.VertScrollVisibility = DevExpress.XtraGrid.Views.Base.ScrollVisibility.Never;
-            // 
-            // myGridControl2
-            // 
-            this.myGridControl2.Location = new System.Drawing.Point(120, 67);
-            this.myGridControl2.MainView = this.myGridView6;
-            this.myGridControl2.Margin = new System.Windows.Forms.Padding(13);
-            this.myGridControl2.Name = "myGridControl2";
-            this.myGridControl2.Size = new System.Drawing.Size(652, 359);
-            this.myGridControl2.TabIndex = 2;
-            this.myGridControl2.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
-            this.myGridView6});
-            // 
-            // myGridView6
-            // 
-            this.myGridView6.GridControl = this.myGridControl2;
-            this.myGridView6.Name = "myGridView6";
-            this.myGridView6.OptionsView.ShowHorizontalLines = DevExpress.Utils.DefaultBoolean.True;
-            this.myGridView6.VertScrollVisibility = DevExpress.XtraGrid.Views.Base.ScrollVisibility.Never;
-            // 
-            // layoutControl1
-            // 
-            this.layoutControl1.Controls.Add(this.textEdit2);
-            this.layoutControl1.Controls.Add(this.myGridControl2);
-            this.layoutControl1.Controls.Add(this.textEdit1);
-            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
-            this.layoutControl1.Name = "layoutControl1";
-            this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(596, 243, 250, 350);
-            this.layoutControl1.Root = this.layoutControlGroup1;
-            this.layoutControl1.Size = new System.Drawing.Size(796, 474);
-            this.layoutControl1.TabIndex = 1;
-            this.layoutControl1.Text = "layoutControl1";
-            // 
-            // textEdit2
-            // 
-            this.textEdit2.Location = new System.Drawing.Point(108, 12);
-            this.textEdit2.Name = "textEdit2";
-            this.textEdit2.Size = new System.Drawing.Size(676, 20);
-            this.textEdit2.StyleController = this.layoutControl1;
-            this.textEdit2.TabIndex = 5;
-            // 
-            // textEdit1
-            // 
-            this.textEdit1.Location = new System.Drawing.Point(120, 430);
-            this.textEdit1.Name = "textEdit1";
-            this.textEdit1.Size = new System.Drawing.Size(652, 20);
-            this.textEdit1.StyleController = this.layoutControl1;
-            this.textEdit1.TabIndex = 4;
-            // 
-            // layoutControlGroup1
-            // 
-            this.layoutControlGroup1.CustomizationFormText = "Root";
-            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
-            this.layoutControlGroup1.GroupBordersVisible = false;
-            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
-            this.layoutControlGroup2,
-            this.layoutControlItem3});
-            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
-            this.layoutControlGroup1.Name = "Root";
-            this.layoutControlGroup1.Size = new System.Drawing.Size(796, 474);
-            this.layoutControlGroup1.Text = "Root";
-            this.layoutControlGroup1.TextVisible = false;
-            // 
-            // layoutControlGroup2
-            // 
-            this.layoutControlGroup2.CustomizationFormText = "layoutControlGroup2";
-            this.layoutControlGroup2.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
-            this.layoutControlItem1,
-            this.layoutControlItem2});
-            this.layoutControlGroup2.Location = new System.Drawing.Point(0, 24);
-            this.layoutControlGroup2.Name = "layoutControlGroup2";
-            this.layoutControlGroup2.Size = new System.Drawing.Size(776, 430);
-            this.layoutControlGroup2.Text = "layoutControlGroup2";
-            // 
-            // layoutControlItem1
-            // 
-            this.layoutControlItem1.Control = this.myGridControl2;
-            this.layoutControlItem1.CustomizationFormText = "layoutControlItem1";
-            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
-            this.layoutControlItem1.Name = "layoutControlItem1";
-            this.layoutControlItem1.Size = new System.Drawing.Size(752, 363);
-            this.layoutControlItem1.Text = "layoutControlItem1";
-            this.layoutControlItem1.TextSize = new System.Drawing.Size(93, 13);
-            // 
-            // layoutControlItem2
-            // 
-            this.layoutControlItem2.Control = this.textEdit1;
-            this.layoutControlItem2.CustomizationFormText = "layoutControlItem2";
-            this.layoutControlItem2.Location = new System.Drawing.Point(0, 363);
-            this.layoutControlItem2.Name = "layoutControlItem2";
-            this.layoutControlItem2.Size = new System.Drawing.Size(752, 24);
-            this.layoutControlItem2.Text = "layoutControlItem2";
-            this.layoutControlItem2.TextSize = new System.Drawing.Size(93, 13);
-            // 
-            // layoutControlItem3
-            // 
-            this.layoutControlItem3.Control = this.textEdit2;
-            this.layoutControlItem3.CustomizationFormText = "layoutControlItem3";
-            this.layoutControlItem3.Location = new System.Drawing.Point(0, 0);
-            this.layoutControlItem3.Name = "layoutControlItem3";
-            this.layoutControlItem3.Size = new System.Drawing.Size(776, 24);
-            this.layoutControlItem3.Text = "layoutControlItem3";
-            this.layoutControlItem3.TextSize = new System.Drawing.Size(93, 13);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1330, 474);
+            this.ClientSize = new System.Drawing.Size(504, 407);
             this.Controls.Add(this.myGridControl1);
-            this.Controls.Add(this.layoutControl1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SingleScrollingGridControl";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.myGridControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.myGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.myGridControl2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.myGridView6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
-            this.layoutControl1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private CustomGrid.MyGridControl myGridControl1;
-        private CustomGrid.MyGridView myGridView1;
-        private DevExpress.XtraLayout.LayoutControl layoutControl1;
-        private CustomGrid.MyGridControl myGridControl2;
-        private CustomGrid.MyGridView myGridView6;
-        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
-        private DevExpress.XtraEditors.TextEdit textEdit1;
-        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
-        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
-        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup2;
-        private DevExpress.XtraEditors.TextEdit textEdit2;
-        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private GridControl myGridControl1;
+        private GridView myGridView1;
     }
 }
 
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,34 +1,4 @@
-﻿' Developer Express Code Central Example:
-' Use only one scroll bar for a grid with multiple master/detail levels.
-' 
-' A grid containing a number of groups (master), each with a number of lines
-' (detail) is a representation of an order which is grouped for readability. When
-' a group has a greater number of lines then fits on the screen, a scrollbar is
-' added to the detail-view and when there are multiple groups the master-view also
-' gets a scrollbar. When the user scrolls through the grid, only the master-view
-' is scrolled (unless a row is selected, then only the detail-view is scrolled)
-' and it feels like the program is behaving oddly, as half the rows are
-' skipped.
-' To achieve this goal, we need the GridControl to have a height equal
-' to the height of all content placed in a master-view. So, the scrollbar does not
-' appear in either the master-view or in the detail-view. To allow scrolling, we
-' need to put our GridControl into a XtraScrollableControl and set the
-' GridControl.Dock property to DockStyle.Top.
-' We created the MyGridControl class
-' - a descendant of the GridControl. MyGridControl works with MyGridView views and
-' includes the CalcGridHeight() method, which is called when the appearance of the
-' Master-View is changed.
-' To put MyGridControl in the XtraScrollableContainer,
-' use the MyGridControl.InitScrolling() method at runtime.
-' Please note that if
-' you want to change Bounds or Dock properties of MyGridControl at runtime, change
-' similar properties of the MyGridControl.ScrollableContainer control.
-' 
-' You can find sample updates and versions for different programming languages here:
-' http://www.devexpress.com/example=E4376
-
-
-Imports Microsoft.VisualBasic
+﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports System.Linq
@@ -44,11 +14,10 @@ Namespace SingleScrollingGridControl
 		Inherits Form
 		Public Sub New()
 			InitializeComponent()
+			myGridView1.OptionsDetail.DetailMode = DevExpress.XtraGrid.Views.Grid.DetailMode.Embedded
 		End Sub
 
 		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			myGridControl1.InitScrolling()
-			myGridControl2.InitScrollingInLayout(layoutControlItem1)
 
 			Dim master As Integer = 20
 			Dim child1 As Integer = 16
@@ -63,7 +32,6 @@ Namespace SingleScrollingGridControl
 				Dim obj As New MyObject(i, "Name " & i.ToString(), childList)
 				list.Add(obj)
 			Next i
-			myGridControl2.DataSource = list
 			myGridControl1.DataSource = list
 		End Sub
 
